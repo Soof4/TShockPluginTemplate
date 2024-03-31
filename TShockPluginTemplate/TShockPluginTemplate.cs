26beafa68f31dcfe2d18086d27ea0b2c4d5fb647
Soofa <emr3.cbc@gmail.com>
2024-03-31 20:34:00 +0300

    Add files via upload

---
+++ TShockPluginTemplate/TShockPluginTemplate.cs
@@ -0,0 +1,31 @@
+﻿using TShockAPI;
+using Terraria;
+using TerrariaApi.Server;
+using System.Configuration;
+
+namespace TShockPluginTemplate
+{
+    [ApiVersion(2, 1)]
+    public class TShockPluginTemplate : TerrariaPlugin
+    {
+        public override string Name => "TShockPluginTemplate";
+        public override string Author => "Soofa";
+        public override string Description => "Description";
+        public override Version Version => new Version(1, 0, 0);
+
+        public TShockPluginTemplate(Main game) : base(game) { }
+
+        public override void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Dispose(bool disposing) {
+            if (disposing) {
+                // Unregister the handlers
+            }
+        }
+    }
+
+}
+
